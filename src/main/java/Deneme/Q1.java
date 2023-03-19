dc5cffba8c17c183764105516c005a8abb30c7b7
elmasyunus <av.yunuselmas@gmail.com>
2023-03-19 11:22:52 +0100

    f

--- src/main/java/Deneme/Q1.java
+++ src/main/java/Deneme/Q1.java
@@ -3,6 +3,8 @@ package Deneme;
 public class Q1 {
     public static void main(String[] args) {
         System.out.println("hello world");
-
+        System.out.println("dunya");
+        System.out.println("evren");
+        System.out.println("hayat");
     }
 }
